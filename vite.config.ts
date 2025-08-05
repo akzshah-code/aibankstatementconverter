b21be2326c3484de63488c97ee8fb93a5119a4cf
akzshah-code <akz.shah@gmail.com>
2025-08-05 19:17:40 +0530

    fix: successful local build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // root directory where index.html lives
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: 'index.html', 
+      input: './index.html', // explicitly point to index.html
     },
   },
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> a344fc05a0de9bbe9f6d6ae72bf81a8e2281c6f9
